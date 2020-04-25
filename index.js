23a6cac23f77938f63002fe384ad318ca75f33c3
mac <mac@esco-3.local>
2020-04-25 09:45:06 +0000

    update

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const fontSize = document.querySelector("#change-font-size")
 const bold = document.querySelector(".bold")
 const fontColor = document.querySelector("#change-font-color")
 const fontType = document.querySelector("#select-font-type")
+const colorsWrapper = document.querySelector(".colors-wrapper")
+const preview = document.querySelector(".preview");
+const close = document.querySelector(".close")
+const save = document.querySelectorAll(".save")
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const editorContent = document.querySelector(".editor-content")
+let overlay = getElement('.overlay');
 
 
 
+
+
+// get image
 function getImage() {
     var file = document.querySelector("input[type=file]").files[0];
     var reader = new FileReader();
@@ -20,7 +31,7 @@ function getImage() {
             const img = document.createElement("img");
             img.src = dataURI;
             img.classList.add("dragable")
-            editor.appendChild(img);
+            editorContent.appendChild(img);
         },
         false
     );
@@ -32,9 +43,102 @@ function getImage() {
 }
 
 
-editor.addEventListener("mouseover", function (event) {
+// clear storage
+function clearStorage() {
+
+    localStorage.clear('postCard')
+
+}
+
+// close modal
+function closeModal() {
+
+    document.querySelector(".modal").classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+// show modal
+function showModal() {
+
+    document.querySelector(".modal").classList.add("active");
+
+    console.log(overlay)
+    overlay.classList.add("active")
+}
 
 
+// get element
+function getElement(name) {
+
+    return document.querySelector(`${name}`);
+}
+// localStorage.clear();
+
+
+
+// check if there is template
+let data = localStorage.getItem("postCard")
+
+if (data && data.length > 0) {
+
+    editor.innerHTML = data;
+}
+
+
+// close modal
+close.addEventListener("click", function () {
+    // modal.classList.remove("active");
+
+    let element = getElement(".modal-content");
+    element.innerHTML = "";
+    closeModal()
+});
+
+
+
+//save content
+save.forEach(item => {
+
+    item.addEventListener("click", function () {
+
+        let content = editor.innerHTML;
+        let modalContent = getElement(".modalContent");
+        // save item to local storage
+        localStorage.setItem("postCard", content, false);
+
+    })
+})
+
+// preview design
+
+preview.addEventListener("click", function () {
+
+
+    const content = getElement('.editor').innerHTML;
+    const modalContent = getElement(".modal-content")
+
+    modalContent.innerHTML = content;
+
+    showModal();
+
+
+})
+
+
+
+// change background color
+colorsWrapper.addEventListener("click", function (event) {
+
+    let color = event.target.id;
+
+    console.log(color)
+    let editorContent = document.querySelector(".editor-content");
+
+    if (color.length > 0) {
+        editorContent.style.backgroundColor = color;
+        console.log("pass ")
+
+    }
 })
 
 fontType.addEventListener("change", function () {
@@ -54,6 +158,8 @@ fontColor.addEventListener("change", function () {
 
     const color = this.value;
 
+    console.log(color)
+
     document.execCommand("foreColor", false, color)
 })
 
@@ -65,6 +171,9 @@ bold.addEventListener("click", function () {
     console.log(editor.innerHTML)
 
 })
+
+
+
 // change font size of text
 
 fontSize.addEventListener("change", function () {
@@ -72,15 +181,6 @@ fontSize.addEventListener("change", function () {
     const value = parseInt(this.value);
 
     document.execCommand("fontSize", false, value)
-})
-
-// change background color
-changeBg.addEventListener("change", function () {
-
-    // get color
-    const color = this.value;
-    // change  background color of editor
-    editor.style.backgroundColor = color;
 })
 
 
